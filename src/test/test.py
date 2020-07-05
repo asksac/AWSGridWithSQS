de49e15a48dcaee9a192c5e32a9d5e66e837a96c
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-07-04 23:48:58 -0500

    Increased bit size for testing

--- src/test/test.py
+++ src/test/test.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from producer.primeNumbers import *
 from worker.factorizeInteger import * 
 
-bits = 10 # default 
+bits = 45 # default 
 
 if len(sys.argv) > 1:
   bits = int(sys.argv[1])
