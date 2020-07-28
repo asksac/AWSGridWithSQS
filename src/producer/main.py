b063dc629521373a0c48747921e53cbbef0a0cde
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-07-28 09:00:23 -0500

    Defined log file and revised messages per second

--- src/producer/main.py
+++ src/producer/main.py
@@ -2,67 +2,68 @@ import logging, time, json, random
 import boto3
 from primeNumbers import generateLargePrime
 
+def main():
+  # Get the service resource
+  sqs = boto3.resource('sqs')
+
+  # get the tasks queue
+  tasks_queue = sqs.get_queue_by_name(QueueName='grid_tasks_queue')
+
+  while True: 
+    st = time.time()
+    dt = 0
+    i = 1
+    queue_entries = []
+    csmt = 0
+
+    while i <= MESSAGES_PER_SECOND: 
+      #bits = random.randint(25, 45) 
+      bits = random.randint(15, 25) 
+      number = generateLargePrime(bits)
+
+      req_obj = dict(input = number, type = 'Decimal')
+      req_json = json.dumps(req_obj)
+
+      queue_entries.append(dict(
+        Id = str(i), 
+        MessageBody = req_json
+      ))
+
+      dt = time.time() - st
+
+      if (i % 10 == 0) or (i >= MESSAGES_PER_SECOND) or (dt >= 1.0):
+        # time to send a batch of messages
+        try: 
+          sm_st = time.time()
+          ack = tasks_queue.send_messages(Entries=queue_entries) 
+          sm_et = time.time()
+          smt = sm_et - sm_st 
+          csmt += smt
+          if ack: 
+            logging.debug(f'Sent a batch of {len(queue_entries)} task messages with {smt}s send message latency')
+          queue_entries = []
+        except Error as e:
+          logging.error('Error sending task messages', exc_info=e)
+
+      dt = time.time() - st
+
+      if (i >= MESSAGES_PER_SECOND) or (dt >= 1.0): 
+        # end of inner while loop
+        logging.info(f'Delivered {i} of {MESSAGES_PER_SECOND} messages in {dt}s, and {csmt}s total send message time')
+        break
+      else: 
+        i += 1
+
+    if dt <= 1.0: 
+      time.sleep(1.0 - dt)
+
 # main
-
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-# Get the service resource
-sqs = boto3.resource('sqs')
+LOG_FILENAME = '/var/log/AWSGridWithSQS/producer-main.log'
+logging.basicConfig(filename=LOG_FILENAME, format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 VISIBILITY_TIMEOUT = 120 # 2 mins
+MESSAGES_PER_SECOND = 600
 
-MESSAGES_PER_SECOND = 170
+main()      
 
-# get the tasks queue
-tasks_queue = sqs.get_queue_by_name(QueueName='grid_tasks_queue')
-
-while True: 
-  st = time.time()
-  dt = 0
-  i = 1
-  queue_entries = []
-  csmt = 0
-
-  while i <= MESSAGES_PER_SECOND: 
-    #bits = random.randint(25, 45) 
-    bits = random.randint(15, 25) 
-    number = generateLargePrime(bits)
-
-    req_obj = dict(input = number, type = 'Decimal')
-    req_json = json.dumps(req_obj)
-
-    queue_entries.append(dict(
-      Id = str(i), 
-      MessageBody = req_json
-    ))
-
-    dt = time.time() - st
-
-    if (i % 10 == 0) or (i >= MESSAGES_PER_SECOND) or (dt >= 1.0):
-      # time to send a batch of messages
-      try: 
-        sm_st = time.time()
-        ack = tasks_queue.send_messages(Entries=queue_entries) 
-        sm_et = time.time()
-        smt = sm_et - sm_st 
-        csmt += smt
-        if ack: 
-          logging.debug(f'Sent a batch of {len(queue_entries)} task messages with {smt}s send message latency')
-        queue_entries = []
-      except Error as e:
-        logging.error('Error sending task messages', exc_info=e)
-
-    dt = time.time() - st
-
-    if (i >= MESSAGES_PER_SECOND) or (dt >= 1.0): 
-      # end of inner while loop
-      logging.info(f'Delivered {i} of {MESSAGES_PER_SECOND} messages in {dt}s, and {csmt}s total send message time')
-      break
-    else: 
-      i += 1
-
-  if dt <= 1.0: 
-    time.sleep(1.0 - dt)
-    
-
-  
+logger.shutdown()
