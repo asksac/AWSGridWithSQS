ec6e3b0ae3de82eed2b6d225a1a849592eff6a8b
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-07-29 23:57:27 -0500

    Separate producer and worker log groups

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,5 +1,13 @@
-resource "aws_cloudwatch_log_group" "cw_log_group" {
-  name = "AWSGridWithSQS-Logs"
+resource "aws_cloudwatch_log_group" "cw_workers_log_group" {
+  name = "AWSGridWithSQS/Logs/Workers"
+
+  tags = {
+    app = "AWSGridWithSQS"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "cw_producers_log_group" {
+  name = "AWSGridWithSQS/Logs/Producers"
 
   tags = {
     app = "AWSGridWithSQS"
