ebb7748973ad3f35ebcd5e36d3add5437eaa04e6
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-08-03 20:08:46 -0500

    Converted to launch templates

--- asg.tf
+++ asg.tf
@@ -16,35 +16,77 @@ resource "aws_security_group" "allow_ssh_sg" {
   }
 }
 
-resource "aws_launch_configuration" "worker_launch_config" {
-  name_prefix                 = "awsgrid-worker-lc-"
-  image_id                    = var.ec2_ami_id
-  instance_type               = "c5.large"
-  spot_price                  = "0.045"
-  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
-  security_groups             = [aws_security_group.allow_ssh_sg.id]
-  key_name                    = var.ec2_ssh_keypair_name
-  associate_public_ip_address = true
-  user_data                  = file("launch_script_worker.sh")
+resource "aws_launch_template" "worker_launch_template" {
+  name_prefix                   = "awsgrid-worker-lc-"
+  description                   = "Worker autoscale launch template"
+
+  cpu_options {
+    core_count                  = 1
+    threads_per_core            = 1
+  }
+  image_id                      = var.ec2_ami_id
+  instance_type                 = "c5.large"
+  instance_market_options {
+    market_type                 = "spot"
+    spot_options {
+      spot_instance_type        = "one-time"
+      max_price                 = "0.045"
+    } 
+  }
+
+  iam_instance_profile {
+    name                        = aws_iam_instance_profile.ec2_instance_profile.name
+  } 
+  key_name                      = var.ec2_ssh_keypair_name
+  #vpc_security_group_ids        = [ aws_security_group.allow_ssh_sg.id ]
+  placement {
+    availability_zone           = var.ec2_subnet_az
+  }
+  network_interfaces {
+    associate_public_ip_address = true
+    delete_on_termination       = true 
+    security_groups             = [ aws_security_group.allow_ssh_sg.id ]
+    subnet_id                   = aws_subnet.ec2_instance_subnet.id
+  }
+
+  monitoring {
+    enabled                     = false
+  }
+
+  user_data                     = filebase64("launch_script_worker.sh")
+
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy       = true
+  }
+
+  tag_specifications {
+    resource_type               = "instance"
+    tags = {
+      app = "AWSGridWithSQS"
+    }
   }
 }
 
 resource "aws_autoscaling_group" "worker_asg" {
   name                  = "awsgrid-with-sqs-worker-asg"
   min_size              = 1
-  max_size              = 20
-  desired_capacity      = 4
+  max_size              = 500
+  desired_capacity      = 2
   health_check_type     = "EC2"
   vpc_zone_identifier   = [aws_subnet.ec2_instance_subnet.id]
-  launch_configuration  = aws_launch_configuration.worker_launch_config.name
+  launch_template {
+    id      = aws_launch_template.worker_launch_template.id
+    version = "$Latest"
+  }
   metrics_granularity   = "1Minute"
   enabled_metrics       = ["GroupDesiredCapacity", "GroupInServiceInstances"]
 
   lifecycle {
     create_before_destroy = true
   }
+
+  depends_on            = [ aws_launch_template.producer_launch_template ]
+
   tag {
     key = "Name"
     value = "awsgrid-workers"
@@ -68,18 +110,54 @@ resource "aws_autoscaling_policy" "workers_decr_policy" {
   autoscaling_group_name = aws_autoscaling_group.worker_asg.name
 }
 
-resource "aws_launch_configuration" "producer_launch_config" {
-  name_prefix                 = "awsgrid-producer-lc-"
-  image_id                    = var.ec2_ami_id
-  instance_type               = "c5.large"
-  spot_price                  = "0.045"
-  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
-  security_groups             = [aws_security_group.allow_ssh_sg.id]
-  key_name                    = var.ec2_ssh_keypair_name
-  associate_public_ip_address = true
-  user_data                  = file("launch_script_producer.sh")
+resource "aws_launch_template" "producer_launch_template" {
+  name_prefix                   = "awsgrid-producer-lc-"
+  description                   = "Producer autoscale launch template"
+
+  cpu_options {
+    core_count                  = 1
+    threads_per_core            = 1
+  }
+  image_id                      = var.ec2_ami_id
+  instance_type                 = "c5.large"
+  instance_market_options {
+    market_type                 = "spot"
+    spot_options {
+      spot_instance_type        = "one-time"
+      max_price                 = "0.045"
+    } 
+  }
+
+  iam_instance_profile {
+    name                        = aws_iam_instance_profile.ec2_instance_profile.name
+  } 
+  key_name                      = var.ec2_ssh_keypair_name
+  #vpc_security_group_ids        = [ aws_security_group.allow_ssh_sg.id ]
+  placement {
+    availability_zone           = var.ec2_subnet_az
+  }
+  network_interfaces {
+    associate_public_ip_address = true
+    delete_on_termination       = true 
+    security_groups             = [ aws_security_group.allow_ssh_sg.id ]
+    subnet_id                   = aws_subnet.ec2_instance_subnet.id
+  }
+
+  monitoring {
+    enabled                     = false
+  }
+
+  user_data                     = filebase64("launch_script_producer.sh")
+
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy       = true
+  }
+
+  tag_specifications {
+    resource_type               = "instance"
+    tags = {
+      app = "AWSGridWithSQS"
+    }
   }
 }
 
@@ -87,16 +165,22 @@ resource "aws_autoscaling_group" "producer_asg" {
   name                  = "awsgrid-with-sqs-producer-asg"
   min_size              = 1
   max_size              = 10
-  desired_capacity      = 2
+  desired_capacity      = 1
   health_check_type     = "EC2"
   vpc_zone_identifier   = [aws_subnet.ec2_instance_subnet.id]
-  launch_configuration  = aws_launch_configuration.producer_launch_config.name
+  launch_template {
+    id      = aws_launch_template.producer_launch_template.id
+    version = "$Latest"
+  }
   metrics_granularity   = "1Minute"
   enabled_metrics       = ["GroupDesiredCapacity", "GroupInServiceInstances"]
 
   lifecycle {
     create_before_destroy = true
   }
+
+  depends_on            = [ aws_launch_template.producer_launch_template ]
+
   tag {
     key = "Name"
     value = "awsgrid-producers"
