e7f3e5271b908e5accf24a0b7b86f5e32898f015
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-08-11 23:03:30 -0500

    Excluding terraform.tfvars from version control

--- variables.tf
+++ variables.tf
@@ -1,14 +1,26 @@
 variable "ec2_vpc_id" {
-  type    = string
-  default = "vpc-7eb0f404"
+  type          = string
+  description   = "Specify the ID of an existing VPC with an internet gateway configured"
+/*
+  # validation requires Terraform CLI v0.13.0
+  validation {
+    condition     = length(var.ec2_vpc_id) > 4 && substr(var.ec2_vpc_id, 0, 4) == "vpc-"
+    error_message = "The vpc id value must be valid, starting with \"vpc-\"."
+  }
+*/
 }
 
-variable "ec2_subnet_az" {
+variable "ec2_subnet_az_1" {
   type    = string
   default = "us-east-1a"
 }
 
+variable "ec2_subnet_az_2" {
+  type    = string
+  default = "us-east-1b"
+}
+
 variable "ec2_ssh_keypair_name" {
   type    = string
-  default = "ssh_keypair_project_vedas"
+  description   = "Specify the name of an existing SSH keypair"
 }
